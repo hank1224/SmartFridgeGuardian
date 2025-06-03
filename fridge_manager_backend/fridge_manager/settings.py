a1e8ab2d85f6030e097dbf653bd7d037133d7eed
hank1224 <92917339+hank1224@users.noreply.github.com>
2025-06-03 16:23:33 +0800

    makemigrations

--- fridge_manager_backend/fridge_manager/settings.py
+++ fridge_manager_backend/fridge_manager/settings.py
@@ -27,7 +27,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key-for-development')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key-for-development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
